b7686289e68a81606cedc096d0cc560c95cef332
jgbondad <jamina.bondad@zalf.de>
2025-10-14 15:00:09 +0200

    corrected consumer

--- run-consumer_avgtradefdays_sm.py
+++ run-consumer_avgtradefdays_sm.py
@@ -140,7 +140,7 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
         "AvgDaysTraDefBelowDST_Stage4": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
         "AvgDaysTraDefBelowDST_Stage5": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
         "AvgDaysTraDefBelowDST_Stage6": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2},
-        # "AvgDaysTraDefBelowDST_Stage7": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2}
+        "AvgDaysTraDefBelowDST_Stage7": {"data": make_dict_nparr(), "cast-to": "float", "digits": 2}
     }
     output_keys = list(output_grids.keys())
 
