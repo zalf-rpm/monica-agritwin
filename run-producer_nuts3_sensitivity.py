fd73ab61ad8c84f08d83ac8e3af5066af36b8ec6
jgbondad <jamina.bondad@zalf.de>
2026-02-11 09:09:58 +0100

    run BW

--- run-producer_nuts3_sensitivity.py
+++ run-producer_nuts3_sensitivity.py
@@ -92,9 +92,44 @@ DATA_SOIL_DB = "germany/buek200.sqlite"
 #DATA_GRID_SLOPE = "germany/HEslope_100_25832_etrs89-utm32n.asc"
 
 # Bavaria
-DATA_GRID_SOIL = "germany/BAVbuek200_100_25832_etrs89-utm32n.asc"
-DATA_GRID_HEIGHT = "BAVdem_100_25832_etrs89-utm32n.asc"
-DATA_GRID_SLOPE = "BAVslope_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SOIL = "germany/BAVbuek200_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_HEIGHT = "BAVdem_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SLOPE = "BAVslope_100_25832_etrs89-utm32n.asc"
+
+# Baden-Württemberg
+DATA_GRID_SOIL = "germany/BWbuek200_100_25832_etrs89-utm32n.asc"
+DATA_GRID_HEIGHT = "germany/BWdem_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SLOPE = "germany/BWslope_100_25832_etrs89-utm32n.asc"
+
+# Mecklenburg-Western Pomerania
+#DATA_GRID_SOIL = "germany/MVbuek200_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_HEIGHT = "germany/MVdem_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SLOPE = "germany/MVslope_100_25832_etrs89-utm32n.asc"
+
+# Rheinhessen-Pfalz
+#DATA_GRID_SOIL = "germany/RPbuek200_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_HEIGHT = "germany/RPdem_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SLOPE = "germany/RPslope_100_25832_etrs89-utm32n.asc"
+
+# Saarland
+#DATA_GRID_SOIL = "germany/SAARbuek200_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_HEIGHT = "germany/SAARdem_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SLOPE = "germany/SAARslope_100_25832_etrs89-utm32n.asc"
+
+# Saxony-Anhalt
+#DATA_GRID_SOIL = "germany/SAbuek200_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_HEIGHT = "germany/SAdem_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SLOPE = "germany/SAslope_100_25832_etrs89-utm32n.asc"
+
+# Schleswig-Holstein
+#DATA_GRID_SOIL = "germany/SHbuek200_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_HEIGHT = "germany/SHdem_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SLOPE = "germany/SHslope_100_25832_etrs89-utm32n.asc"
+
+# Thuringia
+#DATA_GRID_SOIL = "germany/THbuek200_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_HEIGHT = "germany/THdem_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SLOPE = "germany/THslope_100_25832_etrs89-utm32n.asc"
 
 
 TEMPLATE_PATH_LATLON = "{path_to_climate_dir}/latlon-to-rowcol.json"
