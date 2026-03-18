2a6251900fca9989cf25aa6e41e8d9e3429dd4a7
jgbondad <jamina.bondad@zalf.de>
2026-03-18 15:15:15 +0100

    update file paths

--- run-consumer_irrigation.py
+++ run-consumer_irrigation.py
@@ -39,13 +39,14 @@ PATHS = {
     },
     "remoteConsumer-remoteMonica": {
         "path-to-data-dir": "./data/",
+        "path-to-projects-dir": "/project/monica-agritwin/",
         "path-to-output-dir": "/out/out/",
         "path-to-csv-output-dir": "/out/csv-out/"
     }
 }
 
 #TEMPLATE_SOIL_PATH = "{local_path_to_data_dir}germany/raster_backup/buek200_1000_25832_etrs89-utm32n.asc"  # Germany 1000 m
-TEMPLATE_SOIL_PATH = "{local_path_to_data_dir}germany/BWbuek200_100_25832_etrs89-utm32n.asc"  
+TEMPLATE_SOIL_PATH = "{local_path_to_data_dir}germany/BAVbuek200_100_25832_etrs89-utm32n.asc"  
 # TEMPLATE_LANDUSE_PATH = "{local_path_to_data_dir}germany/landuse_1000_31469_gk5.asc"
 DATA_SOIL_DB = "germany/buek200.sqlite"
 USE_LANDUSE = False
@@ -234,7 +235,7 @@ def run_consumer(leave_after_finished_run=True, server={"server": None, "port": 
     leave = False
     write_normal_output_files = False
 
-    path_to_soil_grid = TEMPLATE_SOIL_PATH.format(local_path_to_data_dir=paths["path-to-data-dir"])
+    path_to_soil_grid = TEMPLATE_SOIL_PATH.format(local_path_to_data_dir=paths["path-to-projects-dir"])
     soil_epsg_code = int(path_to_soil_grid.split("/")[-1].split("_")[2])
     soil_crs = CRS.from_epsg(soil_epsg_code)
     soil_metadata, header = Mrunlib.read_header(path_to_soil_grid)
