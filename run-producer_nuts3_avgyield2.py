348f565cfe2e9b4a7e7e5c4580b22899b8d0855e
jgbondad <jamina.bondad@zalf.de>
2026-06-02 18:18:38 +0200

    run LS ww avgyield

--- run-producer_nuts3_avgyield2.py
+++ run-producer_nuts3_avgyield2.py
@@ -75,9 +75,9 @@ DATA_SOIL_DB = "germany/buek200.sqlite"
 #DATA_GRID_IRRIGATION = "germany/irrigation_1000_25832_etrs89-utm32n_other_18.asc"  # potato and sugar beet 2018 irrigation map
 
 # Lower Saxony
-#DATA_GRID_SOIL = "germany/LSbuek200_100_25832_etrs89-utm32n.asc"
-#DATA_GRID_HEIGHT = "germany/LSdem_100_25832_etrs89-utm32n.asc"
-#DATA_GRID_SLOPE = "germany/LSslope_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SOIL = "germany/LSbuek200_100_25832_etrs89-utm32n.asc"
+DATA_GRID_HEIGHT = "germany/LSdem_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SLOPE = "germany/LSslope_100_25832_etrs89-utm32n.asc"
 
 # North Rhine-Westphalia
 #DATA_GRID_SOIL = "germany/NWbuek200_100_25832_etrs89-utm32n.asc"
@@ -100,9 +100,9 @@ DATA_SOIL_DB = "germany/buek200.sqlite"
 #DATA_GRID_SLOPE = "germany/BAVslope_100_25832_etrs89-utm32n.asc"
 
 # Baden-Württemberg
-DATA_GRID_SOIL = "germany/BWbuek200_100_25832_etrs89-utm32n.asc"
-DATA_GRID_HEIGHT = "germany/BWdem_100_25832_etrs89-utm32n.asc"
-DATA_GRID_SLOPE = "germany/BWslope_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SOIL = "germany/BWbuek200_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_HEIGHT = "germany/BWdem_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SLOPE = "germany/BWslope_100_25832_etrs89-utm32n.asc"
 
 # Mecklenburg-Western Pomerania
 #DATA_GRID_SOIL = "germany/MVbuek200_100_25832_etrs89-utm32n.asc"
