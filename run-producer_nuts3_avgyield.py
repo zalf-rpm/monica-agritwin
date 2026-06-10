1629bec6ce717c746816f845f6f39a2198de9950
jgbondad <jamina.bondad@zalf.de>
2026-06-10 11:45:41 +0200

    run BAV avgyield

--- run-producer_nuts3_avgyield.py
+++ run-producer_nuts3_avgyield.py
@@ -95,9 +95,9 @@ DATA_SOIL_DB = "germany/buek200.sqlite"
 #DATA_GRID_SLOPE = "germany/HEslope_100_25832_etrs89-utm32n.asc"
 
 # Bavaria
-#DATA_GRID_SOIL = "germany/BAVbuek200_100_25832_etrs89-utm32n.asc"
-#DATA_GRID_HEIGHT = "germany/BAVdem_100_25832_etrs89-utm32n.asc"
-#DATA_GRID_SLOPE = "germany/BAVslope_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SOIL = "germany/BAVbuek200_100_25832_etrs89-utm32n.asc"
+DATA_GRID_HEIGHT = "germany/BAVdem_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SLOPE = "germany/BAVslope_100_25832_etrs89-utm32n.asc"
 
 # Baden-Württemberg
 #DATA_GRID_SOIL = "germany/BWbuek200_100_25832_etrs89-utm32n.asc"
@@ -130,9 +130,9 @@ DATA_SOIL_DB = "germany/buek200.sqlite"
 #DATA_GRID_SLOPE = "germany/SHslope_100_25832_etrs89-utm32n.asc"
 
 # Thuringia
-DATA_GRID_SOIL = "germany/THbuek200_100_25832_etrs89-utm32n.asc"
-DATA_GRID_HEIGHT = "germany/THdem_100_25832_etrs89-utm32n.asc"
-DATA_GRID_SLOPE = "germany/THslope_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SOIL = "germany/THbuek200_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_HEIGHT = "germany/THdem_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SLOPE = "germany/THslope_100_25832_etrs89-utm32n.asc"
 
 
 TEMPLATE_PATH_LATLON = "{path_to_climate_dir}/latlon-to-rowcol.json"
