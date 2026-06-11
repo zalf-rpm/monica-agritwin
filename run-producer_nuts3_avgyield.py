13809e42f00beec2dc20fb6f0e0592a54fc265e6
jgbondad <jamina.bondad@zalf.de>
2026-06-11 10:08:16 +0200

    run SAX avgyield

--- run-producer_nuts3_avgyield.py
+++ run-producer_nuts3_avgyield.py
@@ -85,9 +85,9 @@ DATA_SOIL_DB = "germany/buek200.sqlite"
 #DATA_GRID_SLOPE = "germany/NWslope_100_25832_etrs89-utm32n.asc"
 
 # Saxony
-#DATA_GRID_SOIL = "germany/SAXbuek200_100_25832_etrs89-utm32n.asc"
-#DATA_GRID_HEIGHT = "germany/SAXdem_100_25832_etrs89-utm32n.asc"
-#DATA_GRID_SLOPE = "germany/SAXslope_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SOIL = "germany/SAXbuek200_100_25832_etrs89-utm32n.asc"
+DATA_GRID_HEIGHT = "germany/SAXdem_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SLOPE = "germany/SAXslope_100_25832_etrs89-utm32n.asc"
 
 # Hessen
 #DATA_GRID_SOIL = "germany/HEbuek200_100_25832_etrs89-utm32n.asc"
@@ -115,9 +115,9 @@ DATA_SOIL_DB = "germany/buek200.sqlite"
 #DATA_GRID_SLOPE = "germany/RPslope_100_25832_etrs89-utm32n.asc"
 
 # Saarland
-DATA_GRID_SOIL = "germany/SAARbuek200_100_25832_etrs89-utm32n.asc"
-DATA_GRID_HEIGHT = "germany/SAARdem_100_25832_etrs89-utm32n.asc"
-DATA_GRID_SLOPE = "germany/SAARslope_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SOIL = "germany/SAARbuek200_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_HEIGHT = "germany/SAARdem_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SLOPE = "germany/SAARslope_100_25832_etrs89-utm32n.asc"
 
 # Saxony-Anhalt
 #DATA_GRID_SOIL = "germany/SAbuek200_100_25832_etrs89-utm32n.asc"
