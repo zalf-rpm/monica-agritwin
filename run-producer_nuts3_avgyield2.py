cdc68f97a2a22720638bc612d3bd3713d3a636e4
jgbondad <jamina.bondad@zalf.de>
2026-06-24 18:52:28 +0200

    run SA avgyield ww

--- run-producer_nuts3_avgyield2.py
+++ run-producer_nuts3_avgyield2.py
@@ -75,9 +75,9 @@ DATA_SOIL_DB = "germany/buek200.sqlite"
 #DATA_GRID_IRRIGATION = "germany/irrigation_1000_25832_etrs89-utm32n_other_18.asc"  # potato and sugar beet 2018 irrigation map
 
 # Lower Saxony
-DATA_GRID_SOIL = "germany/LSbuek200_100_25832_etrs89-utm32n.asc"
-DATA_GRID_HEIGHT = "germany/LSdem_100_25832_etrs89-utm32n.asc"
-DATA_GRID_SLOPE = "germany/LSslope_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SOIL = "germany/LSbuek200_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_HEIGHT = "germany/LSdem_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SLOPE = "germany/LSslope_100_25832_etrs89-utm32n.asc"
 
 # North Rhine-Westphalia
 #DATA_GRID_SOIL = "germany/NWbuek200_100_25832_etrs89-utm32n.asc"
@@ -120,9 +120,9 @@ DATA_GRID_SLOPE = "germany/LSslope_100_25832_etrs89-utm32n.asc"
 #DATA_GRID_SLOPE = "germany/SAARslope_100_25832_etrs89-utm32n.asc"
 
 # Saxony-Anhalt
-#DATA_GRID_SOIL = "germany/SAbuek200_100_25832_etrs89-utm32n.asc"
-#DATA_GRID_HEIGHT = "germany/SAdem_100_25832_etrs89-utm32n.asc"
-#DATA_GRID_SLOPE = "germany/SAslope_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SOIL = "germany/SAbuek200_100_25832_etrs89-utm32n.asc"
+DATA_GRID_HEIGHT = "germany/SAdem_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SLOPE = "germany/SAslope_100_25832_etrs89-utm32n.asc"
 
 # Schleswig-Holstein
 #DATA_GRID_SOIL = "germany/SHbuek200_100_25832_etrs89-utm32n.asc"
