f0e6e2e27ac2c28b1b000db4512e55b03963fe95
jgbondad <jamina.bondad@zalf.de>
2026-08-05 15:04:52 +0200

    Change maps to MV for avgyield sm

--- run-producer_nuts3_avgyield.py
+++ run-producer_nuts3_avgyield.py
@@ -66,9 +66,9 @@ DATA_SOIL_DB = "germany/buek200.sqlite"
 #DATA_GRID_SLOPE = "germany/slope_100_25832_etrs89-utm32n.asc"
 
 # Brandenburg
-DATA_GRID_SOIL = "germany/BBbuek200_100_25832_etrs89-utm32n.asc"
-DATA_GRID_HEIGHT = "germany/BBdem_100_25832_etrs89-utm32n.asc"
-DATA_GRID_SLOPE = "germany/BBslope_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SOIL = "germany/BBbuek200_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_HEIGHT = "germany/BBdem_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SLOPE = "germany/BBslope_100_25832_etrs89-utm32n.asc"
 #DATA_GRID_LAND_USE = "germany/landuse_1000_31469_gk5.asc"
 #DATA_GRID_IRRIGATION = "germany/raster_backup/irrigation_1000_25832_etrs89-utm32n_maize_18.asc"  # maize 2018 irrigation map
 #DATA_GRID_IRRIGATION = "germany/irrigation_1000_25832_etrs89-utm32n_wc_18.asc"  # winter crops 2018 irrigation map
@@ -105,9 +105,9 @@ DATA_GRID_SLOPE = "germany/BBslope_100_25832_etrs89-utm32n.asc"
 #DATA_GRID_SLOPE = "germany/BWslope_100_25832_etrs89-utm32n.asc"
 
 # Mecklenburg-Western Pomerania
-#DATA_GRID_SOIL = "germany/MVbuek200_100_25832_etrs89-utm32n.asc"
-#DATA_GRID_HEIGHT = "germany/MVdem_100_25832_etrs89-utm32n.asc"
-#DATA_GRID_SLOPE = "germany/MVslope_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SOIL = "germany/MVbuek200_100_25832_etrs89-utm32n.asc"
+DATA_GRID_HEIGHT = "germany/MVdem_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SLOPE = "germany/MVslope_100_25832_etrs89-utm32n.asc"
 
 # Rheinhessen-Pfalz
 #DATA_GRID_SOIL = "germany/RPbuek200_100_25832_etrs89-utm32n.asc"
