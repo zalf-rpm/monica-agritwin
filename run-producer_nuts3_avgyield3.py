e89449076815e5dce6aff8ad8cf3c32635761c08
jgbondad <jamina.bondad@zalf.de>
2026-08-07 15:10:17 +0200

    Change maps to NW

--- run-producer_nuts3_avgyield3.py
+++ run-producer_nuts3_avgyield3.py
@@ -80,9 +80,9 @@ DATA_SOIL_DB = "germany/buek200.sqlite"
 #DATA_GRID_SLOPE = "germany/LSslope_100_25832_etrs89-utm32n.asc"
 
 # North Rhine-Westphalia
-#DATA_GRID_SOIL = "germany/NWbuek200_100_25832_etrs89-utm32n.asc"
-#DATA_GRID_HEIGHT = "germany/NWdem_100_25832_etrs89-utm32n.asc"
-#DATA_GRID_SLOPE = "germany/NWslope_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SOIL = "germany/NWbuek200_100_25832_etrs89-utm32n.asc"
+DATA_GRID_HEIGHT = "germany/NWdem_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SLOPE = "germany/NWslope_100_25832_etrs89-utm32n.asc"
 
 # Saxony
 #DATA_GRID_SOIL = "germany/SAXbuek200_100_25832_etrs89-utm32n.asc"
@@ -95,9 +95,9 @@ DATA_SOIL_DB = "germany/buek200.sqlite"
 #DATA_GRID_SLOPE = "germany/HEslope_100_25832_etrs89-utm32n.asc"
 
 # Bavaria
-DATA_GRID_SOIL = "germany/BAVbuek200_100_25832_etrs89-utm32n.asc"
-DATA_GRID_HEIGHT = "germany/BAVdem_100_25832_etrs89-utm32n.asc"
-DATA_GRID_SLOPE = "germany/BAVslope_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SOIL = "germany/BAVbuek200_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_HEIGHT = "germany/BAVdem_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SLOPE = "germany/BAVslope_100_25832_etrs89-utm32n.asc"
 
 # Baden-Württemberg
 #DATA_GRID_SOIL = "germany/BWbuek200_100_25832_etrs89-utm32n.asc"
