3a78fdb54756728e186fc30f74909b0f9f2fc189
jgbondad <jamina.bondad@zalf.de>
2026-08-10 08:37:55 +0200

    Change maps to RP

--- run-producer_nuts3_avgyield.py
+++ run-producer_nuts3_avgyield.py
@@ -75,9 +75,9 @@ DATA_SOIL_DB = "germany/buek200.sqlite"
 #DATA_GRID_IRRIGATION = "germany/irrigation_1000_25832_etrs89-utm32n_other_18.asc"  # potato and sugar beet 2018 irrigation map
 
 # Lower Saxony
-DATA_GRID_SOIL = "germany/LSbuek200_100_25832_etrs89-utm32n.asc"
-DATA_GRID_HEIGHT = "germany/LSdem_100_25832_etrs89-utm32n.asc"
-DATA_GRID_SLOPE = "germany/LSslope_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SOIL = "germany/LSbuek200_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_HEIGHT = "germany/LSdem_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SLOPE = "germany/LSslope_100_25832_etrs89-utm32n.asc"
 
 # North Rhine-Westphalia
 #DATA_GRID_SOIL = "germany/NWbuek200_100_25832_etrs89-utm32n.asc"
@@ -110,9 +110,9 @@ DATA_GRID_SLOPE = "germany/LSslope_100_25832_etrs89-utm32n.asc"
 #DATA_GRID_SLOPE = "germany/MVslope_100_25832_etrs89-utm32n.asc"
 
 # Rheinhessen-Pfalz
-#DATA_GRID_SOIL = "germany/RPbuek200_100_25832_etrs89-utm32n.asc"
-#DATA_GRID_HEIGHT = "germany/RPdem_100_25832_etrs89-utm32n.asc"
-#DATA_GRID_SLOPE = "germany/RPslope_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SOIL = "germany/RPbuek200_100_25832_etrs89-utm32n.asc"
+DATA_GRID_HEIGHT = "germany/RPdem_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SLOPE = "germany/RPslope_100_25832_etrs89-utm32n.asc"
 
 # Saarland
 #DATA_GRID_SOIL = "germany/SAARbuek200_100_25832_etrs89-utm32n.asc"
