27cbaf715f64626962b4090783a525ff5d100cb2
rcescueta <rachel.escueta@gmail.com>
2026-09-15 17:05:20 +0200

    Fix EPSG code extraction

--- run-producer_nuts3_avgyield_irrig.py
+++ run-producer_nuts3_avgyield_irrig.py
@@ -316,7 +316,7 @@ def run_producer(server={"server": None, "port": None}, shared_id=None):
         if setup.get("irrigation_data"):
             DATA_GRID_IRRIGATION = str("germany/irrigation_maps/" + setup["irrigation_data"])
             path_to_irrigation_grid = paths["path-to-projects-dir"] + DATA_GRID_IRRIGATION
-            irrigation_epsg_code = int(path_to_irrigation_grid.split("/")[-1].split("_")[4])
+            irrigation_epsg_code = int(path_to_irrigation_grid.split("/")[-1].split("_")[2])
             irrigation_crs = CRS.from_epsg(irrigation_epsg_code)
             if irrigation_crs not in soil_crs_to_x_transformers:
                 soil_crs_to_x_transformers[irrigation_crs] = Transformer.from_crs(soil_crs, irrigation_crs)
