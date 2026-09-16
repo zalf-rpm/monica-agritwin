3615d34b66517051af0fe6a4e9743584ae313414
jgbondad <jamina.bondad@zalf.de>
2026-09-16 07:13:59 +0200

    Change maps to SA

--- run-producer_nuts3_avgyield_irrig2.py
+++ run-producer_nuts3_avgyield_irrig2.py
@@ -96,9 +96,9 @@ DATA_SOIL_DB = "germany/buek200.sqlite"
 #DATA_GRID_SLOPE = "germany/HEslope_100_25832_etrs89-utm32n.asc"
 
 # Bavaria
-DATA_GRID_SOIL = "germany/BAVbuek200_100_25832_etrs89-utm32n.asc"
-DATA_GRID_HEIGHT = "germany/BAVdem_100_25832_etrs89-utm32n.asc"
-DATA_GRID_SLOPE = "germany/BAVslope_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SOIL = "germany/BAVbuek200_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_HEIGHT = "germany/BAVdem_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SLOPE = "germany/BAVslope_100_25832_etrs89-utm32n.asc"
 
 # Baden-Württemberg
 #DATA_GRID_SOIL = "germany/BWbuek200_100_25832_etrs89-utm32n.asc"
@@ -121,9 +121,9 @@ DATA_GRID_SLOPE = "germany/BAVslope_100_25832_etrs89-utm32n.asc"
 #DATA_GRID_SLOPE = "germany/SAARslope_100_25832_etrs89-utm32n.asc"
 
 # Saxony-Anhalt
-#DATA_GRID_SOIL = "germany/SAbuek200_100_25832_etrs89-utm32n.asc"
-#DATA_GRID_HEIGHT = "germany/SAdem_100_25832_etrs89-utm32n.asc"
-#DATA_GRID_SLOPE = "germany/SAslope_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SOIL = "germany/SAbuek200_100_25832_etrs89-utm32n.asc"
+DATA_GRID_HEIGHT = "germany/SAdem_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SLOPE = "germany/SAslope_100_25832_etrs89-utm32n.asc"
 
 # Schleswig-Holstein
 #DATA_GRID_SOIL = "germany/SHbuek200_100_25832_etrs89-utm32n.asc"
