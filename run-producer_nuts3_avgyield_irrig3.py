c8c7f3d248e1191f1a466c787d7b7d925c1d9f41
jgbondad <jamina.bondad@zalf.de>
2026-09-16 07:15:05 +0200

    update

--- run-producer_nuts3_avgyield_irrig3.py
+++ run-producer_nuts3_avgyield_irrig3.py
@@ -139,7 +139,7 @@ DATA_SOIL_DB = "germany/buek200.sqlite"
 TEMPLATE_PATH_LATLON = "{path_to_climate_dir}/latlon-to-rowcol.json"
 # TEMPLATE_PATH_LATLON = "data/latlon_to_rowcol.json"
 TEMPLATE_PATH_CLIMATE_CSV = "{gcm}/{rcm}/{scenario}/{ensmem}/{version}/{crow}/daily_mean_RES1_C{ccol}R{crow}.csv.gz" #historical
-#TEMPLATE_PATH_CLIMATE_CSV = "{gcm}/{rcm}/{scenario}/{ensmem}/{version}/row-{crow}/col-{ccol}.csv" projection
+#TEMPLATE_PATH_CLIMATE_CSV = "{gcm}/{rcm}/{scenario}/{ensmem}/{version}/row-{crow}/col-{ccol}.csv" #projection
 
 TEMPLATE_PATH_HARVEST = "{path_to_data_dir}/projects/monica-germany/ILR_SEED_HARVEST_doys_{crop_id}.csv"
 
