fc8ac0f63cc726b22948a41eab00d6e15c19f4b5
rcescueta <rachel.escueta@gmail.com>
2026-09-17 17:40:04 +0200

    Update path to latlon

--- run-producer_proj.py
+++ run-producer_proj.py
@@ -136,9 +136,9 @@ DATA_GRID_SLOPE = "germany/BAVslope_100_25832_etrs89-utm32n.asc"
 # DATA_GRID_HEIGHT = "germany/THdem_100_25832_etrs89-utm32n.asc"
 # DATA_GRID_SLOPE = "germany/THslope_100_25832_etrs89-utm32n.asc"
 
-# TEMPLATE_PATH_LATLON = "{path_to_climate_dir}/latlon-to-rowcol.json"
+TEMPLATE_PATH_LATLON = "{path_to_climate_dir}/latlon-to-rowcol.json"
 # TEMPLATE_PATH_LATLON = "data/latlon_to_rowcol.json"
-TEMPLATE_PATH_LATLON = "data/latlon-to-rowcol.json"  # climate projection
+# TEMPLATE_PATH_LATLON = "data/latlon-to-rowcol.json"  # climate projection
 # TEMPLATE_PATH_CLIMATE_CSV = "{gcm}/{rcm}/{scenario}/{ensmem}/{version}/{crow}/daily_mean_RES1_C{ccol}R{crow}.csv.gz"  # historical
 TEMPLATE_PATH_CLIMATE_CSV = "{gcm}/{rcm}/{scenario}/{ensmem}/{version}/row-{crow}/col-{ccol}.csv"  # climate projection
 
