289e3890139b18115bd9ad37e72fafdef51ac001
jgbondad <jamina.bondad@zalf.de>
2026-09-22 19:17:05 +0200

    Change maps to RP

--- run-producer_proj.py
+++ run-producer_proj.py
@@ -112,14 +112,14 @@ DATA_GRID_CROPS_DIR = "germany/raster_proj"
 # DATA_GRID_SLOPE = "germany/MVslope_100_25832_etrs89-utm32n.asc"
 
 # Rheinhessen-Pfalz
-# DATA_GRID_SOIL = "germany/RPbuek200_100_25832_etrs89-utm32n.asc"
-# DATA_GRID_HEIGHT = "germany/RPdem_100_25832_etrs89-utm32n.asc"
-# DATA_GRID_SLOPE = "germany/RPslope_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SOIL = "germany/RPbuek200_100_25832_etrs89-utm32n.asc"
+DATA_GRID_HEIGHT = "germany/RPdem_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SLOPE = "germany/RPslope_100_25832_etrs89-utm32n.asc"
 
 # Saarland
-DATA_GRID_SOIL = "germany/SAARbuek200_100_25832_etrs89-utm32n.asc"
-DATA_GRID_HEIGHT = "germany/SAARdem_100_25832_etrs89-utm32n.asc"
-DATA_GRID_SLOPE = "germany/SAARslope_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SOIL = "germany/SAARbuek200_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_HEIGHT = "germany/SAARdem_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SLOPE = "germany/SAARslope_100_25832_etrs89-utm32n.asc"
 
 # Saxony-Anhalt
 # DATA_GRID_SOIL = "germany/SAbuek200_100_25832_etrs89-utm32n.asc"
