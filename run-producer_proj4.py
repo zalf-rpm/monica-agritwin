6d7d322e43d2c0c468b5df965c6c4f882c5050ab
jgbondad <jamina.bondad@zalf.de>
2026-09-24 07:58:03 +0200

    Change maps to BAV

--- run-producer_proj4.py
+++ run-producer_proj4.py
@@ -87,14 +87,14 @@ DATA_GRID_CROPS_DIR = "germany/raster_proj"
 # DATA_GRID_SLOPE = "germany/NWslope_100_25832_etrs89-utm32n.asc"
 
 # Bavaria
-#DATA_GRID_SOIL = "germany/BAVbuek200_100_25832_etrs89-utm32n.asc"
-#DATA_GRID_HEIGHT = "germany/BAVdem_100_25832_etrs89-utm32n.asc"
-#DATA_GRID_SLOPE = "germany/BAVslope_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SOIL = "germany/BAVbuek200_100_25832_etrs89-utm32n.asc"
+DATA_GRID_HEIGHT = "germany/BAVdem_100_25832_etrs89-utm32n.asc"
+DATA_GRID_SLOPE = "germany/BAVslope_100_25832_etrs89-utm32n.asc"
 
 # Saxony
-DATA_GRID_SOIL = "germany/SAXbuek200_100_25832_etrs89-utm32n.asc"
-DATA_GRID_HEIGHT = "germany/SAXdem_100_25832_etrs89-utm32n.asc"
-DATA_GRID_SLOPE = "germany/SAXslope_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SOIL = "germany/SAXbuek200_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_HEIGHT = "germany/SAXdem_100_25832_etrs89-utm32n.asc"
+#DATA_GRID_SLOPE = "germany/SAXslope_100_25832_etrs89-utm32n.asc"
 
 # Hessen
 # DATA_GRID_SOIL = "germany/HEbuek200_100_25832_etrs89-utm32n.asc"
